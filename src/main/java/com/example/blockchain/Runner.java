996a0a6dda25476a88c8030e5c5d18be967c2734
ayush <ayushmanish@vt.edu>
2022-03-30 16:28:09 -0400

    Timestamp

--- src/main/java/com/example/blockchain/Runner.java
+++ src/main/java/com/example/blockchain/Runner.java
@@ -1,14 +1,17 @@
 package com.example.blockchain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.security.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class Runner implements Runnable{
 
     private static MiningNode[] miningNodes;
 
+    private static User[] users;
+
+    private static Random r = new Random();
     /*
     * Runs:
     * 1. Without Sharding Block Capacity - 256 txns
@@ -25,13 +28,21 @@ public class Runner implements Runnable{
     // Create Users
     public static void setup() {
 
+
+        users = new User[100];
+
+        for(int i = 0; i< users.length; i++ ){
+            users[i] = new User(1000.0);
+        }
+
         miningNodes = new MiningNode[100];
-        int[][] graphAdjaicencyMatrix = new int[100][100];
+        int[][] graphAdjacencyMatrix = new int[100][100];
 
         for(int i =0; i < 100; i++){
-            for(int j=i*10; j<10; j++){
-                if(j!=i){
-                    graphAdjaicencyMatrix[i][j] = 1;
+            for(int j=0; j<10; j++){
+                int idx = r.nextInt(100);
+                if(idx!=i){
+                    graphAdjacencyMatrix[i][idx] = 1;
                 }
             }
         }
@@ -39,7 +50,7 @@ public class Runner implements Runnable{
         for(int i =0; i < 100; i++){
             List<MiningNode> temp = new ArrayList<>();
             for(int j = 0; j < 100; j++){
-                if(graphAdjaicencyMatrix[i][j] == 1) {
+                if(graphAdjacencyMatrix[i][j] == 1) {
                     temp.add(miningNodes[j]);
                 }
             }
@@ -63,9 +74,18 @@ public class Runner implements Runnable{
             public void run(){
                 // Generate Trxn
                 for(int i=0; i<100; i++){
-                    Transaction temp = new Transaction();
-                    miningNodes[i].listeningPort(temp);
+                    int u1Idx = r.nextInt(100);
+                    User u1 = users[u1Idx];
+                    User u2 = users[( u1Idx + r.nextInt(99))%100];
 
+                    Date date = new Date();
+
+                    // getting the object of the Timestamp class
+                    Timestamp ts = new Timestamp(date.getTime());
+
+                    Transaction temp = new Transaction(u1, u2.getPubKey().toString(), ts, r.nextDouble());
+
+                    miningNodes[i].listeningPort(temp);
                 }
             }
         },0,3000);
