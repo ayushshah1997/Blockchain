465636affaef0859a91de48718a3781ea31cfa67
ayush <ayushmanish@vt.edu>
2022-04-02 16:20:05 -0400

    Find nonce

--- src/main/java/com/example/blockchain/MiningNode.java
+++ src/main/java/com/example/blockchain/MiningNode.java
@@ -4,10 +4,8 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.security.Timestamp;
+import java.util.*;
 
 public class MiningNode {
 
@@ -22,26 +20,59 @@ public class MiningNode {
         this.neigbours = neighbours;
     }
 
-    public static void main(String args[]) {}
+    public static void main(String args[]) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        long t = 0;
 
-    public void findNonce() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+
+
+//        System.out.println(checkByte("00".getBytes("UTF-16"), 4));
+//        System.out.println(checkByte("a0".getBytes("UTF-16"), 4));
+//        System.out.println(checkByte("0a".getBytes("UTF-16"), 4));
+//        System.out.println(checkByte("00".getBytes("UTF-16"), 4));
+//        System.out.println(checkByte("00a".getBytes("UTF-16"), 4));
+//        System.out.println(checkByte("00b".getBytes("UTF-16"), 4));
+
+
+        for (int i =0; i < 30; i++) {
+            t += findNonce("c7b6r87r487r487", "ewcy32i8y4398tvby89c4n23");
+            System.out.println(t);
+        }
+        System.out.println("Time taken: " + t/30);
+    }
+
+    public static long findNonce(String prevHash, String merkleRootHash) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        long tStart = new Date().getTime();
         long n = 0;
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
 
-        String prevHash = "";
-        String merkleRootHash = "";
         while(true) {
-            String temp =prevHash + merkleRootHash + n;
-            String hashValue = new String(digest.digest(temp.getBytes("UTF-16")));
+            String temp = prevHash + merkleRootHash + n;
+            byte[] hashValue = digest.digest(temp.getBytes("UTF-16"));
 
-            if(hashValue.startsWith("00000000000")){
+            if(checkByte(hashValue, 3)){
+              break;
+            } else {
+                n++;
+            }
+        }
+        long tEnd = new Date().getTime();
+        return tEnd - tStart;
+
+        //Block b = new Block();
+        //broadcastBlock(b);
+    }
+
+
+    public static boolean checkByte(byte[] buffer, int nonce) {
+        boolean rtn = true;
+        for(int i =0; i < nonce; i++) {
+            //System.out.println(buffer[i]);
+            if( buffer[i] != 48 ) {
+                rtn = false;
                 break;
             }
-            n++;
         }
-
-        Block b = new Block();
-        broadcastBlock(b);
+        return rtn;
     }
 
     public void listeningPort(Transaction t) {
